fac879d003d05d512146c57a5f826da4f5c2ef0f
oliglottis <olivier@polyglottis.org>
2014-08-12 12:48:20 +0200

    Use config files

--- main/content-server.go
+++ main/content-server.go
@@ -2,36 +2,26 @@
 package main
 
 import (
-	"flag"
 	"log"
-	"path/filepath"
 
 	"github.com/polyglottis/content_server/database"
 	"github.com/polyglottis/content_server/operations"
 	"github.com/polyglottis/content_server/server"
+	"github.com/polyglottis/platform/config"
 	"github.com/polyglottis/rpc"
 )
 
-var dbFile = flag.String("db", "content.db", "path to sqlite db file")
-var tcpAddr = flag.String("tcp", ":18982", "TCP address of content server")
-var operationsAddr = flag.String("op-tcp", ":16485", "TCP address of operations RPC server")
-
 func main() {
-	flag.Parse()
 
-	abs, err := filepath.Abs(*dbFile)
+	c := config.Get()
+
+	db, err := database.Open(c.ContentDB)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	db, err := database.Open(abs)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	log.Printf("Content server accessing db file %v", abs)
-
-	main := server.New(server.NewServerDB(db), *tcpAddr)
-	op := operations.NewOpServer(db, *operationsAddr)
+	main := server.New(server.NewServerDB(db), c.Content)
+	op := operations.NewOpServer(db, c.ContentOp)
 	p := rpc.NewServerPair("Content Server", main, op)
 
 	err = p.RegisterAndListen()
